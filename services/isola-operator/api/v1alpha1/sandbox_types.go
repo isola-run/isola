4195f0891df24159c3ea4529f0d29f72836e5ed6
Ben Liderman <18233089+benldrmn@users.noreply.github.com>
2026-01-16 01:54:59 +0200

    add e2e tests to ci. align local dev with modern conventions. consolidate code duplicate. give short names to resources

--- services/isola-operator/api/v1alpha1/sandbox_types.go
+++ services/isola-operator/api/v1alpha1/sandbox_types.go
@@ -138,9 +138,11 @@ type SandboxStatus struct {
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
+// +kubebuilder:resource:shortName=sb
 // +kubebuilder:printcolumn:name="Ready",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status",description="Aggregate readiness"
-// +kubebuilder:printcolumn:name="Reason",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].reason",description="Reason for Ready condition"
+// +kubebuilder:printcolumn:name="Template",type="string",JSONPath=".spec.templateRef.name",description="SandboxTemplate reference"
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:printcolumn:name="Reason",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].reason",priority=1,description="Reason for Ready condition"
 // Sandbox is the Schema for the sandboxes API
 type Sandbox struct {
 	metav1.TypeMeta `json:",inline"`
